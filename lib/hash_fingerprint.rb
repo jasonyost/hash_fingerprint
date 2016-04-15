af8a01dc67c09f813d9ca6429179f17d0d65adbb
Jason Yost <jyost@gocortexlabs.com>
2016-04-15 16:24:51 -0600

    Specs

--- lib/hash_fingerprint.rb
+++ lib/hash_fingerprint.rb
@@ -1,5 +1,85 @@
-require "hash_fingerprint/version"
+require 'hash_fingerprint/version'
+require 'digest/sha2'
 
 module HashFingerprint
-  # Your code goes here...
+    # Public: Returns string representation of object
+    #
+    # object  - A hash, or array
+    #
+    # Examples
+    #  hash = {a: {b: 'b', c: 'c'} d: 'd'}
+    #  HashFingerprint.get_object_string(object)
+    #  => "[\"[\\\"b=>b\\\", \\\"c=>c\\\"]Array=>[\\\"b=>b\\\", \\\"c=>c\\\"]Array\", \"d=>d\"]Array"
+    #
+    # Returns string
+    def self.get_object_string(object)
+        parse(object).tap { |obj| return obj.class == String ? obj : to_string(obj) }
+    end
+
+    # Public: Returns SHA256 fingerprint of object
+    #
+    # object  - A hash, or array
+    #
+    # Examples
+    #  hash = {a: {b: 'b', c: 'c'} d: 'd'}
+    #  HashFingerprint.fingerprint(object)
+    #  => "95045071ee6ce0333d7cd6408962b64a7520d45e0be6161ecd666a30bf7f3706"
+    #
+    # Returns string
+    def self.fingerprint(object)
+      Digest::SHA256.hexdigest get_object_string object
+    end
+
+    # Public: Returns true/false if objects match
+    #
+    # left_object  - A hash, or array
+    # right_object  - A hash, or array
+    #
+    # Examples
+    #  ordered_hash = {a: {b: 'b', c: 'c'} d: 'd'}
+    #  unordered_hash = {:d => 'd', :a => {:c => 'c', :b => 'b'}}
+    #  HashFingerprint.compare(left_object, right_object)
+    #  => true
+    #
+    # Returns bool
+    def self.compare(left_object, right_object)
+      fingerprint(left_object) == fingerprint(right_object)
+    end
+
+    private
+
+    def self.parse(object)
+        case object
+        when Hash
+            parse_hash object
+        when Array
+            parse_array object
+        when String
+            object
+        else
+            to_string(object)
+        end
+    end
+
+    def self.hash_string_array(hash)
+      [].tap do |a|
+        hash.each { |k,v| a << pair_string(k,v) }
+      end
+    end
+
+    def self.pair_string(k, v)
+      "#{get_object_string(v)}=>#{get_object_string(v)}"
+    end
+
+    def self.parse_hash(hash)
+      get_object_string hash_string_array(hash)
+    end
+
+    def self.parse_array(arr)
+      arr.map! { |i| get_object_string i }.sort!
+    end
+
+    def self.to_string(object)
+      "#{object.to_s}#{object.class.to_s}"
+    end
 end
--- spec/hash_fingerprint_spec.rb
+++ spec/hash_fingerprint_spec.rb
@@ -3,11 +3,110 @@ SimpleCov.start
 require 'spec_helper'
 
 describe HashFingerprint do
+  let(:ordered_hash) { { a: { b: 'b', c: 'c' }, d: 'd', e: { f: 1, g: 2 } } }
+  let(:unordered_hash) { { d: 'd', a: { c: 'c', b: 'b' }, e: { g: 2, f: 1 } } }
+  let(:ordered_array) { [1, 2, 3, [4, [5, 6]], %w(a b c)] }
+  let(:unordered_array) { [%w(c b a), 3, 2, 1, [[5, 6], 4]] }
+
   it 'has a version number' do
-    expect(HashFingerprint::VERSION).not_to be nil
+      expect(HashFingerprint::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '.get_object_string(object)' do
+    context 'given a valid argument' do
+        it 'should not raise an error' do
+            expect { subject.get_object_string(ordered_hash) }.not_to raise_error
+        end
+    end
+
+    context 'given a Hash' do
+        it 'should return a string of the hash' do
+            expect(subject.get_object_string ordered_hash).to be_a String
+        end
+
+        it 'should return equal strings regardless of order' do
+            expect(subject.get_object_string ordered_hash).to eq(subject.get_object_string unordered_hash)
+        end
+    end
+
+    context 'given an array' do
+        it 'should should return a string of the array' do
+            expect(subject.get_object_string ordered_array).to be_a String
+        end
+
+        it 'should return equal strings regardless of order' do
+            expect(subject.get_object_string ordered_array).to eq(subject.get_object_string unordered_array)
+        end
+    end
+  end
+
+  describe '.fingerprint(object)' do
+    context 'given a valid argument' do
+        it 'should not raise an error' do
+            expect { subject.fingerprint ordered_hash }.not_to raise_error
+        end
+
+        context 'given a hash' do
+            it 'should return a string' do
+                expect(subject.fingerprint ordered_hash).to be_a String
+            end
+
+            it 'should return an SHA256 fingerprint' do
+                expect(subject.fingerprint ordered_hash).to eq(Digest::SHA256.hexdigest subject.get_object_string ordered_hash)
+            end
+
+            it 'should return equal fingerprints regardless of order' do
+                expect(subject.fingerprint ordered_hash).to eq(subject.fingerprint unordered_hash)
+            end
+        end
+
+        context 'given a array' do
+            it 'should return a string' do
+                expect(subject.fingerprint ordered_array).to be_a String
+            end
+
+            it 'should return an SHA256 fingerprint' do
+                expect(subject.fingerprint ordered_array).to eq(Digest::SHA256.hexdigest subject.get_object_string ordered_array)
+            end
+
+            it 'should return equal fingerprints regardless of order' do
+                expect(subject.fingerprint ordered_array).to eq(subject.fingerprint unordered_array)
+            end
+        end
+    end
+  end
+
+  describe '.compare' do
+    context 'given valid arguments' do
+      it 'should not raise an error' do
+        expect { subject.compare ordered_hash, unordered_hash }.not_to raise_error
+      end
+
+      context 'given matching hashes with with differing orders' do
+        it 'should return true' do
+          expect(subject.compare ordered_hash, unordered_hash).to eq true
+        end
+      end
+
+      context 'given differing hashes' do
+        it 'should return false' do
+          hash = { a: 'a', b: 'b' }
+          expect(subject.compare hash, ordered_hash).to eq false
+        end
+      end
+
+      context 'given matching arrays with with differing orders' do
+        it 'should return true' do
+          expect(subject.compare ordered_array, unordered_array).to eq true
+        end
+      end
+
+      context 'given differing hashes' do
+        it 'should return false' do
+          array = [5, 11, 'a', :b]
+          expect(subject.compare array, ordered_array).to eq false
+        end
+      end
+    end
   end
 end
